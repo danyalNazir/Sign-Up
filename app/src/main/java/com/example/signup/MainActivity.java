91b034b1580d524524b9fbca1d5a2285e12d3780
Danyal <Daniraj107@gmail.com>
2022-05-01 16:16:14 -0700

    Adding validation and toasts

--- app/src/main/java/com/example/signup/MainActivity.java
+++ app/src/main/java/com/example/signup/MainActivity.java
@@ -14,6 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button);
+        TextView username=findViewById(R.id.userName);
+        TextView email=findViewById(R.id.email);
+        TextView password=findViewById(R.id.password);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().length()>0 && email.getText().length()>0 && password.getText().length()>0) {
+                    username.setText("");
+                    email.setText("");
+                    password.setText("");
+                    Toast.makeText(MainActivity.this,"Sign Up Successfully!",Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(MainActivity.this,"Fill out all the fields!",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
